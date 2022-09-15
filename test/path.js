702246610708c821cf3a5c941eaa78980b2dac9a
jcake <jakieschneider13@gmail.com>
2022-09-15 22:39:32 +1000

    Improve tests

--- test/path.js
+++ test/path.js
@@ -3,6 +3,10 @@ import {promises as rl} from "node:readline";
 // import glob, * as path from '../build/ts/src/core/path.js';
 import target, {core} from 'mkjson';
 
+const state = core.config.setState(prev => ({
+    makefilePath: [...prev.makefilePath, process.cwd() + '/bin/makefile.json5']
+}));
+
 const q = rl.createInterface(process.stdin, process.stdout);
 while (true)
     console.log(await q.question('$ ').then(path => core.Path.toAbs(path)));
