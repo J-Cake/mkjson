a813af0da9404e7db6e02984d9b46702272d7f7c
jcake <jakieschneider13@gmail.com>
2022-09-15 14:49:14 +1000

    minor tweaks

--- src/core/plugin.ts
+++ src/core/plugin.ts
@@ -1,9 +1,12 @@
 import {promises as fs} from 'node:fs';
+import chalk from "chalk";
 import StateManager from "@j-cake/jcake-utils/state";
 
 import {TargetList} from "./targetList.js";
 import log from "./log.js";
-import chalk from "chalk";
+import * as API from './plugin-api.js';
+
+export type {TargetList} from './targetList.js';
 
 export type Plugin = Partial<{
     /**
@@ -53,15 +56,38 @@ export interface Glob {
 }
 
 export interface SchemeHandler {
+    /**
+     * Get the time (in milliseconds) a file or resource was last modified.
+     * @param file
+     */
     getMTime(file: string): Promise<Date | number>,
 
+    /**
+     * Get the size of the file or resource in bytes
+     * @param file
+     */
     getSize(file: string): Promise<number>,
 
+    /**
+     * An iterator which lists all files in the specified directory recursively.
+     * * The expected format is an absolute path from the volume root.
+     * @param dir
+     */
     lsDir(dir: string): AsyncIterable<string>,
 
+    /**
+     * Fetch the contents of a file in raw binary format
+     * @param path
+     */
     fetch(path: string): Promise<Buffer>,
-    fetch(path: string, encoding: 'utf8' | 'utf-8' | 'base64' | 'hex'): Promise<string>
-    fetch(path: string, encoding?: 'utf8' | 'utf-8' | 'base64' | 'hex'): Promise<Buffer | string>
+
+    /**
+     * Fetch the contents of a file using a given encoding format
+     * @param path
+     * @param encoding
+     */
+    fetch(path: string, encoding: API.Encoding): Promise<string>
+    fetch(path: string, encoding?: API.Encoding): Promise<Buffer | string>
 }
 
 /**
