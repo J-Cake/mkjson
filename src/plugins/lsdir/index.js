67d69630979a0528616db968634c343f8342f71d
Jacob Schneider <jakieschneider13@gmail.com>
2023-04-18 18:16:31 +0200

    Update index.js

--- src/plugins/lsdir/index.js
+++ src/plugins/lsdir/index.js
@@ -5,8 +5,6 @@ const path = ["cygwin", "win32"].includes(os.platform()) ?
     `${import.meta.url.match(/^file:\/\/\/([a-z]:\/.*)/i)?.[1]}`.replaceAll('/', '\\') :
     `${import.meta.url.match(/^file:\/\/(\/.*)\/[^\/]*$/)?.[1]}`;
 
-console.log("lsdir", path);
-
 const req = module.createRequire(path);
 
 export default req(["cygwin", "win32"].includes(os.platform()) ?
